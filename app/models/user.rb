48ab4f17445b22115fffcdd80c268ca2a4577b88
Kirk Meyer <kirkmeyer03@gmail.com>
2024-08-02 18:08:01 -0700

    Initial commit: user, post, and comment models and migrations

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  validates :username, presence: true, length: { in: 4..20 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+end
